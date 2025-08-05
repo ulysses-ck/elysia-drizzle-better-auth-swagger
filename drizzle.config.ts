f074065f4911636128451e9b944fe71785eea07d
Ulises <ulisesapaza.280@gmail.com>
2025-08-05 15:52:26 -0300

    chore: include auth schema into drizzle-kit config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: ["./src/db/schema.ts", "./src/db/auth-schema.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
