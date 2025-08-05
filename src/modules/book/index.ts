4b24ad9e7db5590e6d5983bd839b9058f12b7dd9
Ulises <ulisesapaza.280@gmail.com>
2025-08-05 14:59:39 -0300

    feat: book controller

---
+++ src/modules/book/index.ts
@@ -0,0 +1,96 @@
+import { createInsertSchema } from "drizzle-typebox";
+
+import { table } from "@/db/schema";
+import Elysia, { t } from "elysia";
+import { db } from "@/db/db";
+import { eq } from "drizzle-orm";
+
+const _createBook = createInsertSchema(table.bookTable, {
+  author: t.String().examples("Edgar Allan Poe"),
+  genre: t.String().examples("Terror"),
+  title: t.String().examples("The Black Cat"),
+});
+
+const createBook = t.Omit(_createBook, ["id"]);
+
+export const book = new Elysia({
+  prefix: "book",
+  tags: ["book"],
+})
+  .get(
+    "/",
+    async () => {
+      const books = await db.query.bookTable.findMany();
+      return books;
+    },
+    {
+      detail: {
+        summary: "List all books",
+      },
+    }
+  )
+  .post(
+    "/",
+    async ({ body }) => {
+      const bookCreated = await db
+        .insert(table.bookTable)
+        .values({
+          ...body,
+        })
+        .returning();
+
+      return bookCreated;
+    },
+    {
+      body: createBook,
+      detail: "Create a book",
+    }
+  )
+  .get(
+    "/:id",
+    async ({ params }) => {
+      const book = await db.query.bookTable.findFirst({
+        where: (bookTable, { eq }) => eq(bookTable.id, params.id),
+      });
+
+      return book;
+    },
+    {
+      params: t.Object({
+        id: t.String(),
+      }),
+      detail: "Get specific book by id",
+    }
+  )
+  .put(
+    "/:id",
+    async ({ params, body }) => {
+      const book = await db
+        .update(table.bookTable)
+        .set(body)
+        .where(eq(table.bookTable.id, params.id))
+        .returning();
+
+      return book;
+    },
+    {
+      params: t.Object({
+        id: t.String(),
+      }),
+      body: createBook,
+      detail: "Update specific book by id",
+    }
+  )
+  .delete(
+    "/:id",
+    async ({ params }) => {
+      const book = await db
+        .delete(table.bookTable)
+        .where(eq(table.bookTable.id, params.id));
+
+      return book;
+    },
+    {
+      detail: `Delete specific book by id`,
+    }
+  );
